3713a2caee30c184adcf6ac0841c555e00bef49b
KleoVor <t9671253993@gmail.com>
2023-04-25 17:02:48 +0300

    addad tests on junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,34 @@
 package ru.netology.service;
 
 import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
 
     @org.junit.Test
-    public void remain() {
+    public void remain1000() {
+        int amount = 1000;
+        int expected = 0;
 
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void remain999() {
+        int amount = 999;
+        int expected = 1;
 
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void remain1001() {
+        int amount = 1001;
+        int expected = 999;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected,actual);
     }
 }
